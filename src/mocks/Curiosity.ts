1ca6e67b9ea63aedb202b74bf7e4b68458695c6e
will <wlymes@gmail.com>
2023-09-02 11:12:25 -0300

    feat: mock

---
+++ src/mocks/Curiosity.ts
@@ -0,0 +1,6 @@
+export const Curiosity = {
+  Title: 'Você sabia?',
+  Subtitle: "O nome 'Star Wars' passou por algumas mudanças!",
+  Description:
+    "Antes de se tornar a icônica saga que conhecemos hoje como 'Star Wars,' o criador George Lucas tinha um título muito diferente em mente para seu épico espacial. Inicialmente, Lucas batizou seu projeto de 'The Star Wars,' com a intenção de criar uma série que homenageasse as aventuras épicas dos antigos contos de fadas e lendas heroicas. Mas, à medida que a produção avançava, o título foi encurtado para 'Star Wars,' e o resto é história. Essa pequena alteração de nome tornou-se um dos títulos mais reconhecíveis e amados na história do cinema. Desde então, 'Star Wars' cresceu para se tornar uma franquia intergaláctica que abrange filmes, séries de TV, livros, quadrinhos e uma base de fãs dedicada em todo o mundo. E tudo começou com a visão de George Lucas e um título que cativou a imaginação de milhões de pessoas. Que a Força esteja com você!",
+}
