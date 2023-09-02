da01880cb5d7017520f1967c7b2b9ed8beae0262
will <wlymes@gmail.com>
2023-09-02 12:21:36 -0300

    feat: home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Fragment } from 'react'
 import About from './components/About'
 import Gallery from './components/Gallery'
+import Planets from './components/Planets'
+import Films from './components/Films'
+import Vehicles from './components/Vehicles'
 
 export default function Home() {
   return (
     <Fragment>
       <About />
       <Gallery />
+      <Planets />
+      <Films />
+      <Vehicles />
     </Fragment>
   )
 }
