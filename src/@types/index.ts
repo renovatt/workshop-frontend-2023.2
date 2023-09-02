ee37222b4156f933347fd739f0f1cc56219d4554
will <wlymes@gmail.com>
2023-09-02 12:21:26 -0300

    feat: types added

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -36,6 +36,42 @@ export type PlanetsProps = {
   url: string
 }
 
+export type VehiclesProps = {
+  name: string
+  model: string
+  manufacturer: string
+  cost_in_credits: string
+  length: string
+  max_atmosphering_speed: string
+  crew: string
+  passengers: string
+  cargo_capacity: string
+  consumables: string
+  vehicle_class: string
+  pilots: any[]
+  films: string[]
+  created: string
+  edited: string
+  url: string
+}
+
+export type FilmsProps = {
+  title: string
+  episode_id: number
+  opening_crawl: string
+  director: string
+  producer: string
+  release_date: string
+  characters: string[]
+  planets: string[]
+  starships: string[]
+  vehicles: string[]
+  species: string[]
+  created: string
+  edited: string
+  url: string
+}
+
 export type RouterLinksProps = {
   text: string
   href: string
