5cc15c8730c857c16ec3637ed839f157bcdb12c6
will <wlymes@gmail.com>
2023-09-02 11:07:48 -0300

    style: style added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        primary: {
+          DEFAULT: '#222223',
+        },
+      },
     },
   },
   plugins: [],
