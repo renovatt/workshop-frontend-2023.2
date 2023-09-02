3066121d0c3f0209d9e612461bff31ce74331a6f
will <wlymes@gmail.com>
2023-09-02 12:20:23 -0300

    feat: use fetch updated

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,10 +1,22 @@
-import { CharacterProps, PlanetsProps } from '@/@types'
-import { getPeopleData, getPlanetsData } from '@/services'
+import {
+  CharacterProps,
+  PlanetsProps,
+  VehiclesProps,
+  FilmsProps,
+} from '@/@types'
+import {
+  getPeopleData,
+  getPlanetsData,
+  getFilmsData,
+  getVehiclesData,
+} from '@/services'
 import { useEffect, useState } from 'react'
 
 export const useFetch = () => {
   const [people, setPeople] = useState<CharacterProps[]>([])
   const [planets, setPlanets] = useState<PlanetsProps[]>([])
+  const [vehicles, setVechicles] = useState<VehiclesProps[]>([])
+  const [films, setFilms] = useState<FilmsProps[]>([])
 
   const handlePeopleDataFetch = async () => {
     const data = await getPeopleData()
@@ -16,13 +28,27 @@ export const useFetch = () => {
     setPlanets(data)
   }
 
+  const handleVehiclesDataFetch = async () => {
+    const data = await getVehiclesData()
+    setVechicles(data)
+  }
+
+  const handleFilmsDataFetch = async () => {
+    const data = await getFilmsData()
+    setFilms(data)
+  }
+
   useEffect(() => {
     handlePeopleDataFetch()
     handlePlanetsDataFetch()
+    handleVehiclesDataFetch()
+    handleFilmsDataFetch()
   }, [])
 
   return {
     people,
     planets,
+    vehicles,
+    films,
   }
 }
